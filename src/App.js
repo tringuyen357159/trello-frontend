163af55e1c1d1bb660a7f495a6c5cfd4a18ce980
tringuyen357159 <tringuyen357159@gmail.com>
2022-04-05 15:39:56 +0700

    frontend -trello

--- src/App.js
+++ src/App.js
@@ -1,9 +1,114 @@
-
+import './App.scss'
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className='app'>
+      <nav className='nav header'>
+        header
+      </nav>
+      <nav className='nav board'>
+        board
+      </nav>
+      <div className='board-columns'>
+        <div className='column'>
+          <header>Brainstorm</header>
+          <ul>
+            <li><img src='https://d1iv5z3ivlqga1.cloudfront.net/wp-content/uploads/2021/01/30155341/fix-bug-1.jpg' /></li>
+            <li>What do you do?</li>
+            <li>What do you do?</li>
+            <li>What do you do?</li>
+            <li>What do you do?</li>
+            <li>What do you do?</li>
+          </ul>
+          <footer>
+            Footer
+          </footer>
+        </div>
+        <div className='column'>
+          <header>Brainstorm</header>
+          <ul>
+            <li><img src='https://d1iv5z3ivlqga1.cloudfront.net/wp-content/uploads/2021/01/30155341/fix-bug-1.jpg' /></li>
+            <li>What do you do?</li>
+            <li>What do you do?</li>
+            <li>What do you do?</li>
+            <li>What do you do?</li>
+            <li>What do you do?</li>
+          </ul>
+          <footer>
+            Footer
+          </footer>
+        </div>
+        <div className='column'>
+          <header>Brainstorm</header>
+          <ul>
+            <li><img src='https://d1iv5z3ivlqga1.cloudfront.net/wp-content/uploads/2021/01/30155341/fix-bug-1.jpg' /></li>
+            <li>What do you do?</li>
+            <li>What do you do?</li>
+            <li>What do you do?</li>
+            <li>What do you do?</li>
+            <li>What do you do?</li>
+          </ul>
+          <footer>
+            Footer
+          </footer>
+        </div>
+        <div className='column'>
+          <header>Brainstorm</header>
+          <ul>
+            <li><img src='https://d1iv5z3ivlqga1.cloudfront.net/wp-content/uploads/2021/01/30155341/fix-bug-1.jpg' /></li>
+            <li>What do you do?</li>
+            <li>What do you do?</li>
+            <li>What do you do?</li>
+            <li>What do you do?</li>
+            <li>What do you do?</li>
+          </ul>
+          <footer>
+            Footer
+          </footer>
+        </div>
+        <div className='column'>
+          <header>Brainstorm</header>
+          <ul>
+            <li><img src='https://d1iv5z3ivlqga1.cloudfront.net/wp-content/uploads/2021/01/30155341/fix-bug-1.jpg' /></li>
+            <li>What do you do?</li>
+            <li>What do you do?</li>
+            <li>What do you do?</li>
+            <li>What do you do?</li>
+            <li>What do you do?</li>
+          </ul>
+          <footer>
+            Footer
+          </footer>
+        </div>
+        <div className='column'>
+          <header>Brainstorm</header>
+          <ul>
+            <li><img src='https://d1iv5z3ivlqga1.cloudfront.net/wp-content/uploads/2021/01/30155341/fix-bug-1.jpg' /></li>
+            <li>What do you do?</li>
+            <li>What do you do?</li>
+            <li>What do you do?</li>
+            <li>What do you do?</li>
+            <li>What do you do?</li>
+          </ul>
+          <footer>
+            Footer
+          </footer>
+        </div>
+        <div className='column'>
+          <header>Brainstorm</header>
+          <ul>
+            <li><img src='https://d1iv5z3ivlqga1.cloudfront.net/wp-content/uploads/2021/01/30155341/fix-bug-1.jpg' /></li>
+            <li>What do you do?</li>
+            <li>What do you do?</li>
+            <li>What do you do?</li>
+            <li>What do you do?</li>
+            <li>What do you do?</li>
+          </ul>
+          <footer>
+            Footer
+          </footer>
+        </div>
+      </div>
     </div>
   );
 }
